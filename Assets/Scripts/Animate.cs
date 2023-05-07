4fcac96198d49f000114bbb88999a7b1349a847e
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2023-05-07 10:05:40 -0700

    Final

--- Assets/Scripts/Animate.cs
+++ Assets/Scripts/Animate.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeColor : MonoBehaviour
+public class Animate: MonoBehaviour
 {
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+            anim.Play("Size");
+        if (Input.GetKeyDown(KeyCode.S))
+            anim.Play("Spin");
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.Play("Hover");
+        if (Input.GetKeyDown(KeyCode.A))
+            anim.Play("ChangeColor");
+
+
     }
 }
